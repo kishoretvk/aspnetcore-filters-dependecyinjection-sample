31144fbca2ca0181c4592705be296d1db359e1be
venkata krishna kishore terli <kishoretvk@hotmail.com>
2017-06-09 01:00:35 -0500

    first commit

---
+++ WebApplication2/WebApplication2/Filters/ResponseFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.service;
+
+namespace WebApplication2.Filters
+{
+    public class ResponseFilter : Attribute, IResourceFilter
+    {
+        private ISimpleRepo _isimpleRepo;
+
+        public ResponseFilter(ISimpleRepo repo)
+        {
+            _isimpleRepo = repo;
+            
+        }
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+          //  throw new NotImplementedException();
+        }
+
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            context.RouteData.Values.Add("token", _isimpleRepo.complexObj("welcome"));
+            context.HttpContext.Items["tokens"] = _isimpleRepo.complexObj("welcome");
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/service/Impl.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.service
+{
+    public class Impl : ISimpleRepo
+    {
+        public samplemodel complexObj(string samplestring)
+        {
+            var obj = new samplemodel()
+            {
+                email = "abc@gmail.com",
+                
+                name = "sampledata",
+                number = 10
+            };
+            return obj;
+        }
+
+        public string samplemethod(string hello)
+        {
+            return "welcome to usa";
+        }
+    }
+}
